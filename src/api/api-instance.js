d97bfebe4c891920de95c2976076af9c78e41546
Nedim Kaçan <nedimkacanofficial@gmail.com>
2023-11-18 07:03:05 +0300

    Revert "Update api-instance.js"
    This reverts commit c3e14765165ce92204f0f9820c73deb172dfd9c4.

--- src/api/api-instance.js
+++ src/api/api-instance.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const mavidev = axios.create({
     baseURL: 'http://mavidev-backend-production.up.railway.app/',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
 })
 
 export default mavidev;
