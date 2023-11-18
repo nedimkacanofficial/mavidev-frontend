2b5f3af44561b17b040f56f7d57a9e90266738a8
Nedim Kaçan <86536657+nedimkacanofficial@users.noreply.github.com>
2023-11-18 06:28:00 +0300

    Update api-instance.js

--- src/api/api-instance.js
+++ src/api/api-instance.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const mavidev = axios.create({
     baseURL: 'http://mavidev-backend-production.up.railway.app/',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    }
 })
 
 export default mavidev;
